464442ec6817c1c19a77c37e1dc857198d0a6e0c
DaScOrPiO <dunni82@yahoo.com>
2022-12-30 05:35:00 +0100

    Feat: App now responsive

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="d-flex flex-column align-items-center justify-content-center">
+      <div className="footer container-fluid d-flex flex-column justify-content-center align-items-center">
+        <div className="some-details container d-flex flex-column justify-content-center align-items-center mt-5">
+          <h1>IP Address Tracker</h1>
+          <p>
+            Ip address tracker is an application that helps track the location
+            of a given Ip address or domain
+          </p>
+        </div>
+
+        <div className="logo-container d-flex">
+          <a href="mailto:dunni82@yahoo.com" className="link">
+            <img src={require("../images/email.png")} alt="" />
+          </a>
+          <a href="https://github.com/DaScOrPiO" className="link">
+            <img src={require("../images/github.png")} alt="" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/faith-oladunni-54ab8920b/"
+            className="link"
+          >
+            <img src={require("../images/linkedin.png")} alt="" />
+          </a>
+          <a href="https://www.twitter.com/oladunni_faith/" className="link">
+            <img src={require("../images/twitter.png")} alt="" />
+          </a>
+        </div>
+        <div className=" d-flex mt-3">
+          <p>
+            This project was created by DaScOrPiO, courtsey of FrontendMentor.io{" "}
+            <hr />{" "}
+            <span className="d-flex align-iten-center justify-content-center text-center">
+              &copy; 2022{" "}
+            </span>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
